5859c5687fb91028c48a7803300e586896a6f0dc
Margherita Grasso <margherita.grasso3@gmail.com>
2022-03-13 23:26:09 +0100

    fix: icon and transition menu

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         </li>
       </ul>
       <div>
-        <p>margherita.grasso3@gmail.com</p>
+        <a href = "mailto: margherita.grasso3@gmail.com">margherita.grasso3@gmail.com</a>
       </div>
     </footer>
   );
